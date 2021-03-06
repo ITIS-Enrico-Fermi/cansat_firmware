7d7baa277ec43189716cfbb8fe06767afdbc19bf
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-03-06 12:06:28 +0100

    SPS30 Query working example

--- src/main.c
+++ src/main.c
@@ -22,8 +22,10 @@
 
 #include "sps30.h"
 
+/*
 FILE *log_stream;
-
+struct sps30_measurement sps30_last_measure;
+EventGroupHandle_t device_barrier;*/
 
 typedef struct {
     int timestamp;
@@ -33,57 +35,64 @@ typedef struct {
     struct sps30_measurement partmatter;
 } Payload_t;
 
-
-struct query_sensors_params {
+struct task_parameters {
     QueueHandle_t pipeline;
     EventGroupHandle_t dev_barrier;
     GPSDevice_t gps_dev;
     QueueHandle_t pm_queue;
 };
-void query_sensors_task(void *params) {
 
-    struct query_sensors_params *parameters = (struct query_sensors_params*)params;
 
-    QueueHandle_t pipeline = parameters->pipeline;
-    EventGroupHandle_t devices_barrier = parameters->dev_barrier;
-    GPSDevice_t gps = parameters->gps_dev;
+
+struct task_parameters task_params;
+
+
+
+void query_sensors_task(void *pvParameters) {
+
+    struct task_parameters *tp = (struct task_parameters *)pvParameters;
+
+    QueueHandle_t pipeline = tp->pipeline;
+    EventGroupHandle_t devices_barrier = tp->dev_barrier;
+    GPSDevice_t gps = tp->gps_dev;
 
     Payload_t payload;
+
+    EventBits_t querying = DEV_BME280 + DEV_GPS + DEV_SPS30;
 
     while(true) {
 
         EventBits_t ready = xEventGroupWaitBits(
             devices_barrier,
-            DEV_SPS30,//DEV_BME280 | DEV_GPS,
+            querying,
             pdTRUE,
             pdFALSE,
             1000
         );
 
-
         if(ready != 0) {
 
-        if(ready && DEV_BME280) {
+        if(ready & DEV_BME280) {
             bme280_data_t ambient = bme280_get_last_measure();
 
             payload.ambient = ambient;
         }
         
-        if(ready && DEV_GPS) {
+        if(ready & DEV_GPS) {
             gps_position_t position;
             gps_get_current_position(gps, &position);
 
             payload.position = position;
         }
 
-        if(ready && DEV_SPS30) {
-            struct sps30_measurement partmatter;
-            xQueueReceive(parameters->pm_queue, &partmatter, 100 / portTICK_RATE_MS);
+        if(ready & DEV_SPS30) {
+            struct sps30_measurement m;
+            xQueueReceive(tp->pm_queue, &m, 1000 / portTICK_RATE_MS);
 
-            payload.partmatter = partmatter;
+            payload.partmatter = m;
         }
 
-        payload.contains = ready;
+        payload.contains = ready & querying;
 
         xQueueSend(pipeline, &payload, 100 / portTICK_PERIOD_MS);
         }
@@ -93,9 +102,10 @@ void query_sensors_task(void *params) {
 }
 
 
-void prepare_payload_task(void *params) {
+void prepare_payload_task(void *pvParameters) {
 
-    QueueHandle_t pipeline = (QueueHandle_t)params;
+    struct task_parameters *tp = (struct task_parameters *)pvParameters;
+    QueueHandle_t pipeline = tp->pipeline;
 
     Payload_t payload;
     char out_buf[1000];
@@ -105,14 +115,14 @@ void prepare_payload_task(void *params) {
         if(xQueueReceive(pipeline, &payload, 1000 / portTICK_PERIOD_MS) == pdTRUE) {
             ESP_LOGI("payload_task", "Received payload");
 
-        if(payload.contains && DEV_BME280) {
+        if(payload.contains & DEV_BME280) {
             bme280_data_t *amb = &payload.ambient;
             sprintf(out_buf, "T: %.2f, P: %.2f, h: %.2f", amb->temperature, amb->pressure, amb->humidity);
             printf("%s\n", out_buf);
             //fprintf(log_stream, "%s\t", out_buf);
         }
 
-        if(payload.contains && DEV_GPS) {
+        if(payload.contains & DEV_GPS) {
             gps_position_t *pos = &payload.position;
             sprintf(
                 out_buf,
@@ -126,7 +136,7 @@ void prepare_payload_task(void *params) {
             //fprintf(log_stream, "%s\n", out_buf);
         }
 
-        if(payload.contains && DEV_SPS30) {
+        if(payload.contains & DEV_SPS30) {
             struct sps30_measurement *pm = &payload.partmatter;
             sprintf(
                 out_buf,
@@ -158,7 +168,9 @@ void sps30_task(void *pvParameters) {
     int16_t ret;
     struct sps30_measurement m;
 
-    struct query_sensors_params *env = (struct query_sensors_params *)pvParameters;
+    struct task_parameters *tp = (struct task_parameters *)pvParameters;
+
+    EventGroupHandle_t devices_barrier = tp->dev_barrier;
 
     sensirion_i2c_select_bus(I2C_NUM_0);
     sensirion_i2c_init();
@@ -182,52 +194,32 @@ void sps30_task(void *pvParameters) {
         ESP_LOGE(TAG, "SPS30 Error starting measurement");
     ESP_LOGD(TAG, "SPS30 Started measurement");
 
-    while(1) {
+    while(true) {
         sensirion_sleep_usec(SPS30_MEASUREMENT_DURATION_USEC);
         ret = sps30_read_measurement(&m);
 
         if(ret < 0) {
             ESP_LOGW(TAG, "SPS30 Cannot retrieve last measurement");
         } else {
-            ESP_LOGI(TAG, "measured values:\n"
-                   "\t%0.2f pm1.0\n"
-                   "\t%0.2f pm2.5\n"
-                   "\t%0.2f pm4.0\n"
-                   "\t%0.2f pm10.0\n"
-                   "\t%0.2f nc0.5\n"
-                   "\t%0.2f nc1.0\n"
-                   "\t%0.2f nc2.5\n"
-                   "\t%0.2f nc4.5\n"
-                   "\t%0.2f nc10.0\n"
-                   "\t%0.2f typical particle size\n",
-                   m.mc_1p0, m.mc_2p5, m.mc_4p0, m.mc_10p0, m.nc_0p5, m.nc_1p0,
-                   m.nc_2p5, m.nc_4p0, m.nc_10p0, m.typical_particle_size);
-
-            //xQueueSend(env->pm_queue, &m, 100 / portTICK_RATE_MS);
-            //xEventGroupSetBits(env->dev_barrier, DEV_SPS30);
+            xQueueSend(tp->pm_queue, &m, 100 / portTICK_RATE_MS);
+            xEventGroupSetBits(devices_barrier, DEV_SPS30);
         }
     }
 
     vTaskDelete(NULL);
 }
 
+
 void app_main() {
-    EventGroupHandle_t device_barrier = xEventGroupCreate();
 
-    QueueHandle_t pipeline = xQueueCreate(10, sizeof(Payload_t));
-    QueueHandle_t pm_queue = xQueueCreate(10, sizeof(struct sps30_measurement));
-
-    ESP_LOGI(TAG, "Let's start our tasks");
-
-    struct query_sensors_params qstask_params = {
-        .pipeline       = pipeline,
-        .dev_barrier    = device_barrier,
-        .gps_dev        = NULL,
-        .pm_queue       = pm_queue
+    task_params = (struct task_parameters){
+        .pipeline       = xQueueCreate(10, sizeof(Payload_t)),
+        .dev_barrier    = xEventGroupCreate(),
+        .pm_queue       = xQueueCreate(10, sizeof(struct sps30_measurement))
     };
-    xTaskCreate(query_sensors_task, "query_sensors", 4096, (void*)&qstask_params, 10, NULL);        //Query sensors
-    xTaskCreate(prepare_payload_task, "prepare_payload", 4096, pipeline, 10, NULL);                 //Prepare payload for tx
 
-    xTaskCreate(sps30_task, "sps30", 4096, (void*)&qstask_params, 10, NULL);
-
+    xTaskCreate(sps30_task, "sps30", 2048, &task_params, 1, NULL);
+    xTaskCreate(query_sensors_task, "query", 2048, &task_params, 1, NULL);
+    xTaskCreate(prepare_payload_task, "payload", 4096, &task_params, 1, NULL);
+    
 }
