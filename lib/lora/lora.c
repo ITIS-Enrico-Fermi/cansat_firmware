e2b7ea188c63f853c2027efee0dd9b925f395643
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-04-21 20:56:22 +0200

    create lora hal

---
+++ lib/lora/lora.c
@@ -0,0 +1,6 @@
+/**
+ * @file lora.c
+ * @author CanSat team
+ * @date 21 Apr 2021
+ * @brief LoRa wrapper for RFM95 (RFM9_) module
+ */
---
+++ lib/lora/lora.h
@@ -0,0 +1,8 @@
+/**
+ * @file lora.h
+ * @author CanSat team
+ * @date 21 Apr 2021
+ * @brief LoRa wrapper for RFM95 (RFM9_) module
+ */
+
+#include "rfm95.h"
