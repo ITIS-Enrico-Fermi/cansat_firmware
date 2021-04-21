851cabf137a7808ab9907c8eef2e950b27536b24
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-04-21 20:59:28 +0200

    adding devices

--- lib/devices/devices.h
+++ lib/devices/devices.h
@@ -14,5 +14,7 @@ typedef enum {
     DEV_IMU     =   (0x01 << 2),
     DEV_SPS30   =   (0x01 << 3),
     DEV_NTC     =   (0x01 << 4),
+    DEV_RFM95   =   (0x01 << 5),
+    DEV_FAN     =   (0x01 << 6),
     DEV_TOTAL_REGISTERED
 } Device_t;
