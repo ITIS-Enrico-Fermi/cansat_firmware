8e0a5cbdcff1ae0b1765fd2c10589a468a01ab15
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-28 11:26:57 +0200

    add controller lib

---
+++ lib/controller/controller.c
@@ -0,0 +1,6 @@
+/**
+ * @file controller.c
+ * @author CanSat team
+ * @date 28 May 2021
+ * @brief FSM implementation for launch sequence management.
+ */
---
+++ lib/controller/controller.h
@@ -0,0 +1,7 @@
+/**
+ * @file controller.h
+ * @author CanSat team
+ * @date 28 May 2021
+ * @brief FSM implementation for launch sequence management.
+ */
+
